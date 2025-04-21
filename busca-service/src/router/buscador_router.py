e9518059e5193b4968a9e17c8afa7e4410d6f244
entr0pie <caiohporcel@gmail.com>
2025-04-21 19:53:01 -0300

    feat: CF-42: adiciona hashtags na busca por tatuadores

--- busca-service/src/router/buscador_router.py
+++ busca-service/src/router/buscador_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from fastapi import Query
 
-from src.service.buscador_service import BuscadorServiceDeps
+from src.service.buscador_service import BuscadorServiceDeps, BuscaTatuadorResultado
 
 router = APIRouter(prefix="/buscador")
 
 @router.get("/buscar", summary="Busca por tatuadores disponíveis no Rabisko.", tags=["Motor de Busca"])
-def buscar_por_tatuadores(service: BuscadorServiceDeps, pesquisa: str = Query()):
-    return service.buscar_tatuadores(pesquisa)
+def buscar_por_tatuadores(service: BuscadorServiceDeps, pesquisa: str = Query(), page: int = Query(1, ge=1), size: int = Query(10, ge=1, le=20)) -> list[BuscaTatuadorResultado]:
+    return service.buscar_tatuadores(pesquisa, page, size)
--- busca-service/src/service/buscador_service.py
+++ busca-service/src/service/buscador_service.py
@@ -3,26 +3,56 @@ from fastapi import Depends
 from src.clients.tokenizador_core_client import TokenizadorCoreClientDep
 from src.model.database import SessionDep
 from sqlmodel import select
-from src.model.models import TatuadorIndiceModel, TatuadorModel
+from sqlalchemy.orm import selectinload
+from src.model.models import TatuadorIndiceModel, TatuadorModel, TatuadorHashtagModel
 from itertools import chain
+from pydantic import BaseModel
+
+class BuscaTatuadorResultado(BaseModel):
+    id: int
+    nome_de_usuario: str
+    nome_de_exibicao: str
+    descricao: str
+    hashtags: list[str]
 
 class BuscadorService:
     def __init__(self, client: TokenizadorCoreClientDep, session: SessionDep):
         self._client = client
         self._session = session
 
-    def buscar_tatuadores(self, pesquisa: str):
+    def buscar_tatuadores(self, pesquisa: str, page: int, size: int) -> list[BuscaTatuadorResultado]:
         tokens = self._client.tokenizar(pesquisa)
         
         documentos = self._session.exec(
             select(TatuadorIndiceModel).where(TatuadorIndiceModel.token.in_(tokens))
         ).all()
 
-        documentos_ids = chain.from_iterable(documento.tatuadores_ids for documento in documentos)
+        documentos_ids = tuple(chain.from_iterable(documento.tatuadores_ids for documento in documentos))
+        
+        # Esse procedimento é CARO. Pós POC: cuidar com muitos selects.
+        stmt = (
+            select(TatuadorModel)
+            .where(TatuadorModel.id.in_(documentos_ids))
+            .order_by(TatuadorModel.id)
+            .limit(size)
+            .offset((page - 1) * size)
+            .options(
+                selectinload(TatuadorModel.hashtags).selectinload(TatuadorHashtagModel.hashtag)
+            )
+        )
 
-        return self._session.exec(
-            select(TatuadorModel).where(TatuadorModel.id.in_(documentos_ids))
-        ).all()
+        tatuadores = self._session.exec(stmt).unique().all()
+
+        return [
+            BuscaTatuadorResultado(
+                id=t.id,
+                nome_de_usuario=t.nome_de_usuario,
+                nome_de_exibicao=t.nome_de_exibicao,
+                descricao=t.descricao,
+                hashtags=[th.hashtag.hashtag for th in t.hashtags]
+            )
+            for t in tatuadores
+        ]
 
 def get_service(client: TokenizadorCoreClientDep, session: SessionDep):
     return BuscadorService(client, session)
